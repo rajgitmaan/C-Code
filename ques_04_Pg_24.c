7d36dbf74e5162b8465ab7327f67c64fb3534333
rajgitmaan <101932656+rajgitmaan@users.noreply.github.com>
2022-03-21 12:34:33 +0530

    Update ques_04_Pg_24.c

--- ques_04_Pg_24.c
+++ ques_04_Pg_24.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
 int main(){
-  printf("yuvraj dahiya\n")
+  printf("yuvraj dahiya\n");
 // Paper of size A0 has dimensions 1189 mm x 841 mm. Each subsequent size A(n)
 //  is defined as A(n-1) cut in half parallel to its shorter sides.
 // Thus paper of size A1 would have dimensions 841 mm x 594 mm.
