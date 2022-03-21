ac9ef69231de3e877fed4c5e00b331f19440cfb1
rajgitmaan <101932656+rajgitmaan@users.noreply.github.com>
2022-03-21 12:36:26 +0530

    Update ques_02_Pg_20.c

--- ques_02_Pg_20.c
+++ ques_02_Pg_20.c
@@ -4,6 +4,7 @@ int main()
 {
     // WAP to calculate gross salary given dearness allowance=>40% of basic salary
     // and house ren allowance=> 20% of salary
+    printf("yuvraj dahiya\n"); 
     int salary, a;
     printf("Your basic salary = ");
     scanf("%d", &salary);
