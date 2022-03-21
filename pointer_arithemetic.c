6ee61a536c6215db9131a873ed361d4e9acc7e68
rajgitmaan <101932656+rajgitmaan@users.noreply.github.com>
2022-03-21 12:37:20 +0530

    Update pointer_arithemetic.c

--- pointer_arithemetic.c
+++ pointer_arithemetic.c
@@ -3,7 +3,7 @@
 int main()
 {
     int arr[3] = {3,4,5}, *ptr = arr;
-    printf("Yugam Grover\n");
+    printf("Yuvraj dahiya\n");
     for(int i = 0; i < 3; i++)
     {
         printf("element %d of array = %d, Address = %d\n", i+1, *ptr, ptr);
