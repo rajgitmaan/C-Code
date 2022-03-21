fc0f7afd586025450f02d9e559714ee3c7fcf2ff
rajgitmaan <101932656+rajgitmaan@users.noreply.github.com>
2022-03-21 12:19:22 +0530

    Update 01_pointers_practice.c

--- 01_pointers_practice.c
+++ 01_pointers_practice.c
@@ -3,7 +3,7 @@
 int main()
 {
     int a, b, sum;
-    printf("Yugam Grover\n");
+    printf("Yuvraaj Dahiya\n");
     printf("\n");
     printf("Enter the value of a and b\n");
     scanf("%d %d", &a,&b);
